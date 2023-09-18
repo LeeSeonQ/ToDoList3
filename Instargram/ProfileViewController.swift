9279e647b50f244d24d7e81818b5d41abd25e0e5
LeeSeonQ <lsk021181@gmail.com>
2023-09-18 10:26:18 +0900

    [Feat]: 섹션 구현하기 후 섹션별 셀 구현

--- Instargram/ProfileViewController.swift
+++ Instargram/ProfileViewController.swift
@@ -2,40 +2,20 @@
 //  ProfileViewController.swift
 //  Instargram
 //
-//  Created by Lee on 2023/09/13.
+//  Created by Lee on 2023/09/18.
 //
 
 import UIKit
-import SnapKit
-import Then
-
 
 class ProfileViewController: UIViewController {
 
-    private let userNameLabel = UILabel().then {
-        $0.text = "UserName"
-        $0.font = UIFont.systemFont(ofSize: 18)
-        $0.textAlignment = .center
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.backgroundColor = .black
-        $0.textColor = .white
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    func setUpUI() {
-        view.addSubview(userNameLabel)
-        
-        userNameLabel.snp.makeConstraints {
-            $0.width.equalTo(view.frame.width * 0.4)
-            $0.height.equalTo(44)
-            $0.top.equalToSuperview().offset(50)
-            $0.centerX.equalToSuperview()
-        }
-    }
+    
+
     /*
     // MARK: - Navigation
 
